b489b1113e5bdd069164edf1fc4ca86ae23bacc9
freedom-wy <dazhuang_python@sina.com>
2021-12-19 19:22:17 +0800

    搜索参数验证

--- app/web/book_view.py
+++ app/web/book_view.py
@@ -1,20 +1,27 @@
-from flask import jsonify
+from flask import jsonify, request
 from helper import is_isbn_or_key
 from yushu_book import YuShuBook
 from .blueprint import book_bp
+from app.forms.args_verification import searchArgsVerification
 
 
-@book_bp.route("/book/search/<q>/<page>")
-def search(q, page):
+@book_bp.route("/book/search")
+def search():
     """
     搜索图书
     :param q: 搜索关键字或搜索的ISBN号码, ISBN10为10位数字,可能包含-,ISBN13为13位数字
     :param page: 页码
     :return:
     """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == "isbn":
-        result = YuShuBook.search_by_isbn(q)
+    search_args_verification = searchArgsVerification(request.args)
+    if search_args_verification.validate():
+        q = search_args_verification.q.data.strip()
+        page = search_args_verification.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == "isbn":
+            result = YuShuBook.search_by_isbn(q)
+        else:
+            result = YuShuBook.search_by_keyword(q)
+        return jsonify(result)
     else:
-        result = YuShuBook.search_by_keyword(q)
-    return jsonify(result)
+        return jsonify({"message": "搜索参数校验失败"})
