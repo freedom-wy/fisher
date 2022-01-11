c8fa3aa577840408a35ae4a378ed051ba9c43f63
freedom-wy <1780292537@qq.com>
2022-01-11 14:10:16 +0800

    完成礼物清单,心愿清单,详情页

--- app/libs/db_utils.py
+++ app/libs/db_utils.py
@@ -1,5 +1,5 @@
 # 精妙代码
-from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy, BaseQuery
 from contextlib import contextmanager
 
 
@@ -18,4 +18,12 @@ class SQLAlchemy(_SQLAlchemy):
             raise e
 
 
-db = SQLAlchemy()
+# 所有的filter_by方法中需要传入status=1,因此需要重写filter_by方法
+class Query(BaseQuery):
+    def filter_by(self, **kwargs):
+        if 'status' not in kwargs.keys():
+            kwargs["status"] = 1
+        return super(Query, self).filter_by(**kwargs)
+
+
+db = SQLAlchemy(query_class=Query)
